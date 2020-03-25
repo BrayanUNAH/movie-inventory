c3f79bf8c1682577073719f290474a76f471ccff
BrayanUNAH <bmedinae@unah.hn>
2020-03-25 16:25:41 -0600

    Se agregó la clase movie y la lista enlazada

---
+++ Classes.py
@@ -0,0 +1,24 @@
+class Movie:
+    def __init__(self, movieName, durationTime, directorName, category, description):
+        self.movieName = movieName
+        self.durationTime = durationTime
+        self.directorName = directorName
+        self.category = category
+        self.description = description
+
+class Node:
+    def __init__(self, data):
+        self.next = None
+        self.data = data
+
+class LinkedList:
+    def __init__(self):
+        self.first = None
+
+    def push(self):
+        pass
+
+    def remove(self):
+        pass
+
+    
