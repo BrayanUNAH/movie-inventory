81b82d1aeb32395fd594d262507bf97dcc41ae35
BrayanUNAH <bmedinae@unah.hn>
2020-03-29 23:07:35 -0600

    Se agregó métodos sin terminar para editar o eliminar un elemento de la lista de películas

--- EditWindow.py
+++ EditWindow.py
@@ -1,16 +1,22 @@
 # -*- coding: utf-8 -*-
 
 from PyQt5 import QtCore, QtGui, QtWidgets
-
+from LinkedList import LinkedList
+from AddWindow import AddWindow
+from Movie import Movie
 
 class EditWindow(object):
+
+    def __init__(self, movieList):
+        self.movieList = movieList
+
     def setupUi(self, Dialog):
         Dialog.setObjectName("Dialog")
         Dialog.resize(653, 366)
         self.treeWidget = QtWidgets.QTreeWidget(Dialog)
         self.treeWidget.setGeometry(QtCore.QRect(20, 20, 611, 171))
         self.treeWidget.setObjectName("treeWidget")
-        for i in range(2):
+        for i in range(self.movieList.getTotalItems()):
             item_0 = QtWidgets.QTreeWidgetItem(self.treeWidget)
         self.labelItemNumber = QtWidgets.QLabel(Dialog)
         self.labelItemNumber.setGeometry(QtCore.QRect(100, 220, 151, 20))
@@ -20,7 +26,7 @@ class EditWindow(object):
         font = QtGui.QFont()
         font.setPointSize(28)
         self.textItemNumber.setFont(font)
-        self.textItemNumber.setPlainText("d")
+        self.textItemNumber.setPlainText("")
         self.textItemNumber.setObjectName("textItemNumber")
         self.buttonEditItem = QtWidgets.QPushButton(Dialog)
         self.buttonEditItem.setGeometry(QtCore.QRect(290, 250, 81, 61))
@@ -28,9 +34,81 @@ class EditWindow(object):
         self.buttonDeleteItem = QtWidgets.QPushButton(Dialog)
         self.buttonDeleteItem.setGeometry(QtCore.QRect(410, 250, 81, 61))
         self.buttonDeleteItem.setObjectName("buttonDeleteItem")
-
+        self.buttonEditItem.clicked.connect(self.editItem)
+        self.buttonDeleteItem.clicked.connect(self.deleteItem)
         self.retranslateUi(Dialog)
         QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+    def editItem(self):
+        try:
+            itemNumber = int(self.textItemNumber.toPlainText())
+        except ValueError:
+            self.invalidMessage()
+            return False
+        if itemNumber < self.movieList.getTotalItems() and itemNumber >= 0:
+            self.addWindow(self.movieList.search(itemNumber), itemNumber)
+        else:
+            self.invalidMessage()
+        return True
+
+    def invalidMessage(self):
+        self.message = QtWidgets.QMessageBox()
+        self.message.setWindowTitle("No válido")
+        self.message.setText("Número de Item no válido")
+        x = self.message.exec_()
+
+
+    def addWindow(self, movie, itemNumber):
+        self.window = QtWidgets.QMainWindow()
+        self.ui = AddWindow(self.movieList)
+        self.ui.setupUi(self.window)
+
+        self.ui.textNameMovie.setPlainText(movie.value.getMovieName())
+        self.ui.textDurationTime.setPlainText(movie.value.getMovieDuration())
+        self.ui.textDirector.setPlainText(movie.value.getDirectorName())
+        self.ui.textDescription.setPlainText(movie.value.getDescription())
+
+        self.ui.buttonAdd.hide()
+        
+        self.ui.auxButtonAdd = QtWidgets.QPushButton(self.window)
+        self.ui.auxButtonAdd.setGeometry(QtCore.QRect(374, 440, 101, 41))
+        self.ui.auxButtonAdd.setObjectName("auxButtonAdd")
+        self.ui.auxButtonAdd.setText("Agregar")
+        self.window.show()
+        self.OverwriteMovie(itemNumber)
+        self.ui.auxButtonAdd.clicked.connect(self.ui.clearTextBox)
+
+    def OverwriteMovie(self, itemNumber):
+        movieName = self.ui.textNameMovie.toPlainText()
+        movieDuration = self.ui.textDurationTime.toPlainText()
+        directorName = self.ui.textDirector.toPlainText()
+        category = self.ui.category.currentText()
+        description = self.ui.textDescription.toPlainText()
+
+        if movieName == "" or movieDuration == "" or directorName == "" or directorName == "" or category == "" or description == "":
+            self.message = QtWidgets.QMessageBox()
+            self.message.setWindowTitle("Incompleto")
+            self.message.setText("Todos los campos deben estar completos")
+            x = self.message.exec_()
+            return False
+        movie = Movie(movieName, movieDuration,
+                      directorName, category, description)
+        self.movieList.overwrite(itemNumber, movie)
+        return True
+
+
+    def deleteItem(self):
+        try:
+            itemNumber = int(self.textItemNumber.toPlainText())
+        except ValueError:
+            self.invalidMessage()
+            return False
+        if itemNumber < self.movieList.getTotalItems() and itemNumber >= 0:
+            self.movieList.remove(itemNumber)
+        else:
+            self.invalidMessage()
+        return True
+
 
     def retranslateUi(self, Dialog):
         _translate = QtCore.QCoreApplication.translate
@@ -43,7 +121,10 @@ class EditWindow(object):
         self.treeWidget.headerItem().setText(5, _translate("Dialog", "Categoría"))
         __sortingEnabled = self.treeWidget.isSortingEnabled()
         self.treeWidget.setSortingEnabled(False)
-        self.treeWidget.topLevelItem(0).setText(0, _translate("Dialog", "Item"))
+        for i in range (self.movieList.getTotalItems()):
+            for y in range(0,6):
+                    self.treeWidget.topLevelItem(i).setText(y, _translate("Dialog", "%s" % self.movieList.getTotalItems()))
+        """self.treeWidget.topLevelItem(0).setText(0, _translate("Dialog", "Item"))
         self.treeWidget.topLevelItem(0).setText(1, _translate("Dialog", "nombre"))
         self.treeWidget.topLevelItem(0).setText(2, _translate("Dialog", "duración"))
         self.treeWidget.topLevelItem(0).setText(3, _translate("Dialog", "descripción esto es una description"))
@@ -54,7 +135,7 @@ class EditWindow(object):
         self.treeWidget.topLevelItem(1).setText(2, _translate("Dialog", "sd"))
         self.treeWidget.topLevelItem(1).setText(3, _translate("Dialog", "sd"))
         self.treeWidget.topLevelItem(1).setText(4, _translate("Dialog", "sd"))
-        self.treeWidget.topLevelItem(1).setText(5, _translate("Dialog", "sd"))
+        self.treeWidget.topLevelItem(1).setText(5, _translate("Dialog", "sd"))"""
         self.treeWidget.setSortingEnabled(__sortingEnabled)
         self.labelItemNumber.setText(_translate("Dialog", "No. de Item a Editar o Eliminar"))
         self.buttonEditItem.setText(_translate("Dialog", "Editar"))
